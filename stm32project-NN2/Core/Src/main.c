cf5a2cf3ba6b97e27f54a0bf2125a442f998cbff
janieblas <nibj961116@gmail.com>
2025-10-23 21:42:57 +0000

    new files structure

--- stm32project-NN2/Core/Src/main.c
+++ stm32project-NN2/Core/Src/main.c
@@ -5,16 +5,8 @@
   ******************************************************************************
   */
 #include "main.h"
+#include "system_config.h"
 #include "nn_driver.h"
-
-/* Private variables */
-UART_HandleTypeDef UartHandle;
-
-/* Private function prototypes */
-static void SystemClock_Config(void);
-static void UART_Config(void);
-static void Error_Handler(void);
-int __io_putchar(int ch);
 
 /**
   * @brief  Main program
@@ -41,101 +33,7 @@ int main(void)
     /* Main loop */
     while (1) 
     { 
-        //...
+        // Main application loop
+        // Puedes agregar más lógica aquí
     }
 }
-
-/**
-  * @brief  Retargets printf to USART
-  */
-int __io_putchar(int ch)
-{
-    HAL_UART_Transmit(&UartHandle, (uint8_t *)&ch, 1, 0xFFFF); 
-    return ch;
-}
-
-/**
-  * @brief  System Clock Configuration
-  */
-void SystemClock_Config(void)
-{
-    RCC_ClkInitTypeDef RCC_ClkInitStruct;
-    RCC_OscInitTypeDef RCC_OscInitStruct;
-    HAL_StatusTypeDef ret = HAL_OK;
-
-    __HAL_RCC_PWR_CLK_ENABLE();
-    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-    
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-    RCC_OscInitStruct.HSICalibrationValue = 0x10;
-    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-    RCC_OscInitStruct.PLL.PLLM = 16;
-    RCC_OscInitStruct.PLL.PLLN = 360;
-    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-    RCC_OscInitStruct.PLL.PLLQ = 7;
-    RCC_OscInitStruct.PLL.PLLR = 6;
-    
-    if(HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-    {
-        Error_Handler();
-    }
-    
-    ret = HAL_PWREx_EnableOverDrive();
-    if(ret != HAL_OK)
-    {
-        while(1) { ; }
-    }
-
-    RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | 
-                                   RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
-    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;  
-    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;  
-    
-    if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
-    {
-        Error_Handler();
-    }
-}
-
-/**
-  * @brief  Configure UART peripheral
-  */
-static void UART_Config(void)
-{
-    UartHandle.Instance          = USARTx;
-    UartHandle.Init.BaudRate     = 9600;
-    UartHandle.Init.WordLength   = UART_WORDLENGTH_8B;
-    UartHandle.Init.StopBits     = UART_STOPBITS_1;
-    UartHandle.Init.Parity       = UART_PARITY_NONE;
-    UartHandle.Init.HwFlowCtl    = UART_HWCONTROL_NONE;
-    UartHandle.Init.Mode         = UART_MODE_TX_RX;
-    UartHandle.Init.OverSampling = UART_OVERSAMPLING_16;
-    
-    if(HAL_UART_Init(&UartHandle) != HAL_OK)
-    {
-        Error_Handler(); 
-    }
-}
-
-/**
-  * @brief  Error Handler
-  */
-static void Error_Handler(void)
-{
-    while(1)
-    {
-    }
-}
-
-#ifdef USE_FULL_ASSERT
-void assert_failed(uint8_t* file, uint32_t line)
-{ 
-    while (1)
-    {
-    }
-}
-#endif
